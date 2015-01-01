1bce0e9da5cb7a980f1c5463f6eeb439b00db14b
Tomo Masakura <tomo.masakura@gmail.com>
2015-01-01 21:40:42 +0900

    Implement PageObject extend method.

--- foo.js
+++ foo.js
@@ -1,7 +1,38 @@
 window.testmator = (function () {
   'use strict';
 
+  var extend = function (protoProps, staticProps) {
+    var parent = this;
+    var child = function () {
+      return parent.apply(this, arguments);
+    };
+
+    // Inherit static properties.
+    _.extend(child, parent, staticProps);
+
+    var Surrogate = function () {
+      this.constructor = child;
+    };
+    Surrogate.prototype = parent.prototype;
+    child.prototype = new Surrogate();
+
+    // Inherit prototype properties.
+    if (protoProps) {
+      _.extend(child, protoProps);
+    }
+
+    return child;
+  };
+
+  var PageObject = function () {
+  };
+
+  _.extend(PageObject, {
+    extend: extend
+  });
+
   return _.extend({}, {
-    name: 'testmator'
+    name: 'testmator',
+    PageObject: PageObject
   });
 })();
